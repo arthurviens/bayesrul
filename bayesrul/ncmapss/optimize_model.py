Added code for numerical tests + some fixes

bayesrul/ncmapss/optimize_model.py
@@ -6,14 +6,16 @@ from bayesrul.inference import (
 )
 
 from bayesrul.ncmapss.dataset import NCMAPSSDataModule
+from bayesrul.utils.miscellaneous import simple_cull, select_pareto
 from pathlib import Path
 
 import torch
 import pyro
 import argparse
 import optuna
+import json
 
-debug = True
+debug = False
 EPOCHS = 150 if not debug else 2
 GPU = 2
 device = torch.device(f'cuda:{GPU}')
@@ -26,13 +28,14 @@ def mfvi_objective(trial: optuna.trial.Trial) -> float:
     q_scale = trial.suggest_float("q_scale", 1e-4, 1e-1, log=True)
     fit_context = trial.suggest_categorical("fit_context", ['lrt', 'flipout']) 
     lr = trial.suggest_float("lr", 1e-4, 1e-1, log=True)
-    guide = trial.suggest_categorical("guide", ['normal', 'radial'])
+    guide = "normal"
     optimizer = trial.suggest_categorical("optimizer", ['adam', 'nadam', 'sgd'])
     args.archi = trial.suggest_categorical("args.archi", 
             ['linear', 'conv', 'inception', 'bigception'])
     args.activation = trial.suggest_categorical("args.activation", 
         ['leaky_relu', 'tanh', 'relu'])
-    args.model_name = f'mfvi{trial.number}'
+    args.pretrain = 5 #trial.suggest_categorical("args.pretrain", [0, 10])
+    args.model_name = f'mfvi{trial.number:03d}'
         
     data = NCMAPSSDataModule(args.data_path, batch_size=10000)
     
@@ -46,6 +49,7 @@ def mfvi_objective(trial: optuna.trial.Trial) -> float:
         'guide' : guide,
         'optimizer' : optimizer,
         'pretrain_file' : None,
+        'pretrain': args.pretrain,
         'trial_id' : trial.number,
         'device' : device,
     }
@@ -60,11 +64,83 @@ def mfvi_objective(trial: optuna.trial.Trial) -> float:
     
 def radial_objective(trial: optuna.trial.Trial) -> float:
     pyro.clear_param_store()
-    raise NotImplementedError('Not implemented yet')
+
+    prior_scale = trial.suggest_float("prior_scale", 1e-4, 1, log=True)
+    q_scale = trial.suggest_float("q_scale", 1e-4, 1e-1, log=True)
+    fit_context = trial.suggest_categorical("fit_context", ['lrt', 'flipout']) 
+    lr = trial.suggest_float("lr", 1e-4, 1e-1, log=True)
+    guide = "radial"
+    optimizer = trial.suggest_categorical("optimizer", ['adam', 'nadam', 'sgd'])
+    args.archi = trial.suggest_categorical("args.archi", 
+            ['linear', 'conv', 'inception', 'bigception'])
+    args.activation = trial.suggest_categorical("args.activation", 
+        ['leaky_relu', 'tanh', 'relu'])
+    args.pretrain = 5 # trial.suggest_categorical("args.pretrain", [0, 10])
+    args.model_name = f'radial{trial.number:03d}'
+        
+    data = NCMAPSSDataModule(args.data_path, batch_size=10000)
+    
+    hyperparams = {
+        'bias' : True,
+        'prior_loc' : 0,
+        'prior_scale' : prior_scale,
+        'q_scale' : q_scale,
+        'fit_context' : fit_context,
+        'lr' : lr,
+        'guide' : guide,
+        'optimizer' : optimizer,
+        'pretrain': args.pretrain,
+        'pretrain_file' : None,
+        'trial_id' : trial.number,
+        'device' : device,
+    }
+
+    monitors = ["mse/val", "rmsce/val"]
+    inference = vi_bnn.VI_BNN(args, data, hyperparams, GPU=GPU, studying=True)
+    res = inference.fit(EPOCHS, monitors=monitors)
+
+    return res[0], res[1]
+
     
 def lowrank_objective(trial: optuna.trial.Trial) -> float:
     pyro.clear_param_store()
-    raise NotImplementedError('Not implemented yet')
+
+    prior_scale = trial.suggest_float("prior_scale", 1e-4, 1, log=True)
+    q_scale = trial.suggest_float("q_scale", 1e-4, 1e-1, log=True)
+    fit_context = trial.suggest_categorical("fit_context", ['lrt', 'flipout']) 
+    lr = trial.suggest_float("lr", 1e-4, 1e-1, log=True)
+    guide = "lowrank"
+    optimizer = trial.suggest_categorical("optimizer", ['adam', 'nadam', 'sgd'])
+    args.archi = trial.suggest_categorical("args.archi", 
+            ['inception', 'bigception'])
+    args.activation = trial.suggest_categorical("args.activation", 
+        ['leaky_relu', 'tanh', 'relu'])
+    args.pretrain = 5 #trial.suggest_categorical("args.pretrain", [0, 10])
+    args.model_name = f'lowrank{trial.number:03d}'
+        
+    data = NCMAPSSDataModule(args.data_path, batch_size=10000)
+    
+    hyperparams = {
+        'bias' : True,
+        'prior_loc' : 0,
+        'prior_scale' : prior_scale,
+        'q_scale' : q_scale,
+        'fit_context' : fit_context,
+        'lr' : lr,
+        'guide' : guide,
+        'optimizer' : optimizer,
+        'pretrain' : args.pretrain,
+        'pretrain_file' : None,
+        'trial_id' : trial.number,
+        'device' : device,
+    }
+
+    monitors = ["mse/val", "rmsce/val"]
+    inference = vi_bnn.VI_BNN(args, data, hyperparams, GPU=GPU, studying=True)
+    res = inference.fit(EPOCHS, monitors=monitors)
+
+    return res[0], res[1]
+    
     
 def mcdropout_objective(trial: optuna.trial.Trial) -> float:
     raise NotImplementedError('Not implemented yet')
@@ -110,6 +186,8 @@ if __name__ == "__main__":
         objective = mfvi_objective
     elif args.model == "radial":
         objective = radial_objective
+    elif args.model == "lowrank":
+        objective = lowrank_objective
     elif args.model == "mcmc":
         objective = mcmc_objective
     elif args.model == "mc_dropout":
@@ -120,8 +198,8 @@ if __name__ == "__main__":
         objective = heteroscedasticnn_objective
     else:
         raise ValueError(
-            f"Unknown model '{args.model}'. Try 'mfvi', 'radial', 'mcmc', "
-            "'mc_dropout', 'deep_ensemble', 'hetero_nn'."
+            f"Unknown model '{args.model}'. Try 'mfvi', 'radial', 'lowrank"
+            "'mcmc', 'mc_dropout', 'deep_ensemble', 'hetero_nn'."
             )
 
     sampler = optuna.samplers.RandomSampler()
@@ -139,10 +217,27 @@ if __name__ == "__main__":
     )
     study.optimize(
         objective,
-        n_trials=2, 
+        n_trials=300, 
         timeout=None,
         catch=(RuntimeError,),
     )
     
     print("Number of finished trials: {}".format(len(study.trials)))
-    
+
+    df = study.trials_dataframe()
+    df.dropna(axis=0, inplace=True)
+    pareto, dominated = simple_cull(df[['values_0', 'values_1']].values.tolist())
+    pareto = select_pareto(df, pareto)
+
+    p = Path('results/ncmapss/best_models/', args.study_name)
+    p.mkdir(exist_ok=True)
+    pareto.sort_values('values_0', inplace=True)
+    pareto.reset_index(drop=True, inplace=True)
+    for i, row in pareto.iterrows():
+        n = row['number']
+        #string = json.dumps(, indent=4)
+        with open(Path(p, f'{i:03d}.json').as_posix(), 'w') as f:
+            params = study.trials[n].params
+            params['value_0'] = study.trials[n].values[0]
+            params['value_1'] = study.trials[n].values[1]
+            json.dump(params, f, indent=4)

bayesrul/utils/miscellaneous.py
@@ -1,9 +1,12 @@
 import os, glob
 import torch
-import torch.nn as nn
 
+import torch.nn as nn
+import numpy as np
 import pytorch_lightning as pl
+
 from pytorch_lightning.utilities import rank_zero_only
+
 
 
 class Dotdict(dict):
@@ -81,3 +84,43 @@ def numel(m: torch.nn.Module, only_trainable: bool = False):
         parameters = [p for p in parameters if p.requires_grad]
     unique = {p.data_ptr(): p for p in parameters}.values()
     return sum(p.numel() for p in unique)
+
+
+def simple_cull(inputPoints):
+    def dominates(row, candidateRow):
+        return sum([row[x] <= candidateRow[x] for x in range(len(row))]) == len(row)    
+    
+    paretoPoints = set()
+    candidateRowNr = 0
+    dominatedPoints = set()
+    while True:
+        candidateRow = inputPoints[candidateRowNr]
+        inputPoints.remove(candidateRow)
+        rowNr = 0
+        nonDominated = True
+        while len(inputPoints) != 0 and rowNr < len(inputPoints):
+            row = inputPoints[rowNr]
+            if dominates(candidateRow, row):
+                # If it is worse on all features remove the row from the array
+                inputPoints.remove(row)
+                dominatedPoints.add(tuple(row))
+            elif dominates(row, candidateRow):
+                nonDominated = False
+                dominatedPoints.add(tuple(candidateRow))
+                rowNr += 1
+            else:
+                rowNr += 1
+
+        if nonDominated:
+            # add the non-dominated point to the Pareto frontier
+            paretoPoints.add(tuple(candidateRow))
+
+        if len(inputPoints) == 0:
+            break
+
+    return paretoPoints, dominatedPoints
+
+
+def select_pareto(df, paretoSet):
+    arr = np.array([[x1, x2] for x1, x2 in list(paretoSet)])
+    return df[(df.values_0.isin(arr[:, 0])) & (df.values_1.isin(arr[:, 1]))]